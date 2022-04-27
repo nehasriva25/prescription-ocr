3fbf43b644bb5ab6cd30a80007bf705b2b89d887
nehasriva25 <92880024+nehasriva25@users.noreply.github.com>
2022-04-27 13:16:07 -0400

    Adding Components

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';  
+import './App.css';
+function Header(){
+    return (
+    <div className = "AppHeader">
+    <p>Upload Prescription</p>
+    </div>); 
+}
+export default Header;
